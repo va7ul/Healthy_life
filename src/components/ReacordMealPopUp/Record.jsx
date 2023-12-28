248bd7221ec72f084db01f580e1d3c8685618c1c
VorontsovaYulia <vorontsovayulia89@gmail.com>
2023-12-28 20:21:55 +0200

    Update Record.jsx

--- src/components/ReacordMealPopUp/Record.jsx
+++ src/components/ReacordMealPopUp/Record.jsx
@@ -76,7 +76,7 @@ export const Record = ({
   };
 
   return (
-    <BackDrop>
+    <BackDrop onClick={onCloseHandler}>
       <ContentBlock>
         <Title>Record your meal</Title>
         <MealContainer
