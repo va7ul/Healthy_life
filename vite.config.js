4bdf537c5c0c33a7110109e36ce6d869f7b0e320
va7ul <vas7ul@gmail.com>
2023-12-28 14:25:12 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-      plugins: [react(), svgr()],
-      resolve: {
-        alias: {
-          src: '/src',
-          components: '/src/components',
-          pages: '/src/pages',
-          assets: '/src/assets'
-        },
-      },
-      base: '/Healthy_life/',
-    });
+  plugins: [react(), svgr()],
+  optimizeDeps: { exclude: ['swiper/vue', 'swiper/types'] },
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      pages: '/src/pages',
+      assets: '/src/assets',
+    },
+  },
+
+  base: '/Healthy_life/',
+});
