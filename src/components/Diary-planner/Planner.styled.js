71598c9dd901866ace2edc5fb9a71e51d1836d23
1evanter <rita.oleksenkoo@gmail.com>
2023-12-28 18:14:25 +0200

    Update Planner.styled.js

--- src/components/Diary-planner/Planner.styled.js
+++ src/components/Diary-planner/Planner.styled.js
@@ -1,4 +1,35 @@
+import { Link } from 'react-router-dom';
 import { styled } from 'styled-components';
+
+export const StyledLink = styled(Link)``;
+
+export const BackIcon = styled.svg`
+  display: inline-block;
+  width: 16px;
+  height: 16px;
+  rotate: 180deg;
+  fill: var(--primary-color-grey);
+
+  &:hover {
+    fill: var(--primary-color-green-lite);
+  }
+
+  @media screen and (min-width: 834px) {
+    width: 24px;
+    height: 24px;
+  }
+`;
+
+export const TitleWrap = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  margin-bottom: 16px;
+
+  @media screen and (min-width: 834px) {
+    gap: 12px;
+  }
+`;
 
 export const Title = styled.h2`
   font-size: 24px;
