6c9f33c647d4815ac6c6a3b2b92a87225f6d93c0
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-01-29 10:12:53 +0200

    fix_PageNav

--- src/components/PageNav/PageNav.jsx
+++ src/components/PageNav/PageNav.jsx
@@ -1,11 +1,9 @@
-// import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
 import { NavBox, StyledLink } from "./PageNav.styled";
 
 export const PageNav = () => {
-    // const [active, setActive] = useState("Home");
+    
     return (
-        <NavBox  onClick={(event)=>setActive(event.target.textContent)}>
+        <NavBox>
             <StyledLink to="/main">
                 Home
             </StyledLink>
