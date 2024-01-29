9789034512acc453a81aacbbd5f43e37a71b38c3
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-01-29 10:08:14 +0200

    added_css_for_PageNav

--- src/components/PageNav/PageNav.styled.js
+++ src/components/PageNav/PageNav.styled.js
@@ -1,7 +1,29 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 export const NavBox = styled.nav`
-  display: flex;
-  gap: 40px;
-  margin-right: 60px;
+  display: none;
+  @media screen and (min-width: 1440px) {
+    display: flex;
+    gap: 40px;
+    margin-right: 60px;
+  }
+`;
+
+export const StyledLink = styled(NavLink)`
+  position: relative;
+  padding-top: 0px;
+  padding-bottom: 4px;
+  transition: color var(--trans-dur-and-func);
+
+  &.active::after {
+    position: absolute;
+    left: 0;
+    bottom: -4px;
+    content: '';
+    width: 28px;
+    height: 2px;
+    background: var(--primary-color-green-lite);
+    border-radius: 2px;
+  }
 `;
