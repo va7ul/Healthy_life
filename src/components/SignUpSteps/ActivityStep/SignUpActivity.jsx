ee6a2de71e320d6c725f9549d06fa2c7191deab6
va7ul <vas7ul@gmail.com>
2024-01-30 15:59:06 +0200

    Updated activity checkbox

--- src/components/SignUpSteps/ActivityStep/SignUpActivity.jsx
+++ src/components/SignUpSteps/ActivityStep/SignUpActivity.jsx
@@ -35,26 +35,24 @@ function ActivityLevel({ onBackPage, onForm }) {
             <RadioWrapper>
               <CustomLabel>
                 <CustomRadio type="radio" name="activity" value="1.2" />
-                1.2 - if you do not have physical activity and sedentary work
+                If you do not have physical activity and sedentary work
               </CustomLabel>
               <CustomLabel>
                 <CustomRadio type="radio" name="activity" value="1.375" />
-                1.375 - if you do short runs or light gymnastics 1-3 times a
-                week
+                If you do short runs or light gymnastics 1-3 times a week
               </CustomLabel>
               <CustomLabel>
                 <CustomRadio type="radio" name="activity" value="1.55" />
-                1.55 - if you play sports with average loads 3-5 times a week
+                If you play sports with average loads 3-5 times a week
               </CustomLabel>
               <CustomLabel>
                 <CustomRadio type="radio" name="activity" value="1.725" />
-                1.725 - if you train fully 6-7 times a week
+                If you train fully 6-7 times a week
               </CustomLabel>
               <CustomLabel>
                 <CustomRadio type="radio" name="activity" value="1.9" />
-                1.9 - if your work is related to physical labor, you train 2
-                times a day and include strength exercises in your training
-                program
+                If your work is related to physical labor, you train 2 times a
+                day and include strength exercises in your training program
               </CustomLabel>
             </RadioWrapper>
             <ErrorMessage name="activity" component={StyledErrorMessage} />
