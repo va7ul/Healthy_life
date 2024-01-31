5c2bccdf4873743210d5694336a930b9bf418843
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-01-31 19:40:57 +0200

    fixed_backdrop

--- src/components/HeaderComponents/MobileContainer/MobileContainer.styled.js
+++ src/components/HeaderComponents/MobileContainer/MobileContainer.styled.js
@@ -52,8 +52,8 @@ export const MobileMenu = styled.div`
   background-color: var(--primary-color-black-two);
 
   @media screen and (min-width: 834px) {
-    /* top: 245px;
-    left: 178px; */
+    top: 245px;
+    left: 190px;
     width: 326px;
     height: 286px;
     padding: 58px 40px;
@@ -69,12 +69,17 @@ export const Backdrop = styled.div`
   width: 100vw;
   background-color: rgba(0, 0, 0, 0.4);
   position: fixed;
-  top: 0;
-  left: 0;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   display: flex;
   align-items: center;
   justify-content: center;
   z-index: 1100;
+
+  @media screen and (min-width: 834px) {
+    width: 834px;
+  }
 `;
 
 export const DivMenu = styled.div`
